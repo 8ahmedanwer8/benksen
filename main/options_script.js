e03086087e2911218e18dc9996a088c53b930f7c
8ahmedanwer8 <84689304+8ahmedanwer8@users.noreply.github.com>
2021-06-26 23:47:27 -0400

    Delete options_script.js

--- main/options_script.js
+++
@@ -1,8 +0,0 @@
-console.log(" I am the options");
-chrome.runtime.sendMessages({
-    message: 'get_name'
-}, response =>{
-    if (response.message === 'success'){
-        document.querySelector('div').innerHTML = `Hello ${response.payload}`;
-    }
-});
